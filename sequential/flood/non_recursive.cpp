233a1cc6930094599c9b299d9270162a0a78482d
niltonlqjr <niltonlqjr@gmail.com>
2025-02-08 22:01:10 -0300

    print

--- sequential/flood/non_recursive.cpp
+++ sequential/flood/non_recursive.cpp
@@ -1,20 +1,24 @@
 #include <vips/vips8>
 #include <iostream>
+#include <vector>
+
 using namespace vips;
 
+#define INQUEUE -2
+/*
 VImage *maxtree(VImage *in){
     VImage *parents = NULL;
     return parents;
-}
+}*/
 
-void print_VImage(VImage *in){
+void print_VImage_band(VImage *in, int band = 0){
     double p;
     int h=in->height();
     int w=in->width();
 
     for(int l=0; l < h; l++){
         for(int c=0; c < w; c++){
-            p = in->getpoint(c,l)[0];
+            p = in->getpoint(c,l)[band];
             std::cout.width(4);
             std::cout << p;
         }
@@ -23,6 +27,36 @@ void print_VImage(VImage *in){
 
     return;
 }
+
+void print_matrix(std::vector<std::vector<int>*> *m){
+    
+    for(int l=0;l<m->size(); l++){
+        for(int c=0;c<m->at(l)->size(); c++){
+            std::cout.width(4);
+            std::cout << m->at(l)->at(c);
+        }
+        std::cout << "\n";
+    }
+}
+
+
+std::vector<std::vector<int>*> *maxtree(VImage *in){
+    std::vector<std::vector<int>*> *parent;
+    parent = new std::vector<std::vector<int>*>;
+    int h=in->height();
+    int w=in->width();
+    for(int l=0; l<h; l++){
+        std::vector<int> *line;
+        line = new std::vector<int>;
+        for(int c=0;c<w;c++){
+            line->push_back(-1);
+        }
+        parent->push_back(line);
+    }
+    print_matrix(parent);
+    return parent;
+}
+
 
 
 int main(int argc, char **argv){
@@ -33,10 +67,8 @@ int main(int argc, char **argv){
 
     in = new VImage(VImage::new_from_file(argv[1],NULL));
 
-    print_VImage(in);
-    
-    
-
+    print_VImage_band(in);
+    maxtree(in);
     vips_shutdown();
     std::cout << "\n";
     return 0;
