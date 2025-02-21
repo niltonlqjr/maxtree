1abf6484ec558d9e7e4c98f4830aba155c3f274a
niltonlqjr <niltonlqjr@gmail.com>
2025-02-21 18:31:48 -0300

    non recursive errado

--- sequential/flood/non_recursive.cpp
+++ sequential/flood/non_recursive.cpp
@@ -7,6 +7,38 @@
 
 using namespace vips;
 
+template<typename T>
+class custom_priority_queue : public std::priority_queue<T, std::vector<T>>
+{
+  public:
+
+      bool remove(const T& value) {
+          //auto it= std::find(this->c.begin(), this->c.end(), value);
+          auto it = this->c.begin(); 
+          while(it != this->c.end()){
+            if(*it == value){
+                break;
+            }
+            it++;
+          }
+
+          if (it == this->c.end()) {
+              return false;
+          }
+          if (it == this->c.begin()) {
+              // deque the top element
+              this->pop();
+          }    
+          else {
+              // remove element and re-heap
+              this->c.erase(it);
+              std::make_heap(this->c.begin(), this->c.end(), this->comp);
+         }
+         return true;
+     }
+};
+
+
 #define INQUEUE -2
 /*
 VImage *maxtree(VImage *in){
@@ -18,16 +50,37 @@ class maxtree_node{
     public:
         int parent;
         int area;
+        unsigned int idx;
         bool correct_filter;
         double out_value;
         double gval;
 
-    maxtree_node(double g, double v = 0){
+    maxtree_node(double g, unsigned int i, double v = 0){
         this->parent = -1;
+        this->idx = i;
 //        this->area = 1;
         this->correct_filter = false;
         this->out_value = v;
         this->gval = g;
+    }
+
+    bool operator>(const maxtree_node &r){
+        return this->gval > r.gval;
+    }
+    bool operator>=(const maxtree_node &r){
+        return this->gval >= r.gval;
+    }
+    bool operator<(const maxtree_node &r){
+        return this->gval < r.gval;
+    }
+    bool operator<=(const maxtree_node &r){
+        return this->gval <= r.gval;
+    }
+    bool operator==(const maxtree_node &r){
+        return this->gval == r.gval;
+    }
+    bool operator!=(const maxtree_node &r){
+        return this->gval != r.gval;
     }
 };
 
@@ -57,7 +110,7 @@ void print_VImage_band(VImage *in, int band = 0){
     return;
 }
 
-void print_matrix(std::vector<maxtree_node> *m, int  h, int w){
+void print_matrix(std::vector<maxtree_node*> *m, int  h, int w){
     std::cout << h << ", " << w << "\n";
     std::cout << m->size()<<"\n";
     int l,c;
@@ -66,37 +119,45 @@ void print_matrix(std::vector<maxtree_node> *m, int  h, int w){
         for(c=0;c<w; c++){
             std::cout << "("<< l << "," << c <<")";
             std::cout.width(4);
-            std::cout << m->at(index_of(c,l,h,w)).parent;
+            std::cout << m->at(index_of(c,l,h,w))->parent;
         }
         std::cout << "\n";
     }
 }
 
-std::vector<unsigned int> get_neighbours(unsigned int pixel, int h, int w){
-    std::vector<unsigned int> v;
-    unsigned int pl, pc;
-    std::tie(pl, pc) = lin_col(pixel, h, w);
+std::vector<maxtree_node*> get_neighbours(maxtree_node *pixel, 
+                                          std::vector<maxtree_node *> *t,
+                                          unsigned int h, unsigned int w){
+    std::vector<maxtree_node*> v;
+    unsigned int idx, pl, pc;
+    std::tie(pl, pc) = lin_col(pixel->idx, h, w);
     if(pl < (unsigned int)h - 1){
-        v.push_back(index_of(pl+1, pc, h, w));
+        idx = index_of(pl+1, pc, h, w);
+        v.push_back(t->at(idx));
     }
     if(pl >= 1){
-        v.push_back(index_of(pl-1, pc, h, w));
+        idx = index_of(pl-1, pc, h, w);
+        v.push_back(t->at(idx));
     }
     if(pc < (unsigned int)w - 1){
-        v.push_back(index_of(pl, pc+1, h, w));
+        idx = index_of(pl, pc+1, h, w);
+        v.push_back(t->at(idx));
     }
     if(pc >= 1){
-        v.push_back(index_of(pl, pc-1, h, w));
+        idx = index_of(pl, pc-1, h, w);
+        v.push_back(t->at(idx));
     }
     return v;
 }
 
 
-unsigned int min_gval(std::vector<maxtree_node> *t){
-    unsigned int i, min = 0;
+maxtree_node *min_gval(std::vector<maxtree_node*> *t){
+    unsigned int i; 
+    maxtree_node *min = t->at(0);
+    
     for(i=1; i < t->size(); i++){
-        if (t->at(i).gval < t->at(min).gval){
-            min = i;
+        if (t->at(i)->gval < min->gval){
+            min = t->at(i);
         }
     }
     return min;
@@ -105,76 +166,84 @@ unsigned int min_gval(std::vector<maxtree_node> *t){
 
 
 
-std::vector<maxtree_node> *maxtree(VImage *in, int band = 0){
-    std::vector<maxtree_node> *data;
-    data = new std::vector<maxtree_node>;
+std::vector<maxtree_node*> *maxtree(VImage *in, int band = 0){
+    std::vector<maxtree_node*> *data;
+    data = new std::vector<maxtree_node*>;
     int h=in->height();
     int w=in->width();
     std::vector<bool> *visited = new std::vector<bool>;
-    std::priority_queue<unsigned int> idx_pq;
-    std::stack<unsigned int> idx_stack;
-    unsigned int nextpix, smallest_idx, p,  t, stack_top;
+    custom_priority_queue<maxtree_node*> pixel_pq;
+    std::stack<maxtree_node*> pixel_stack;
+    maxtree_node *nextpix, *p, *t, *stack_top;
 
-
+    unsigned int idx=0;
     for(int l=0; l<h; l++){
         for(int c=0;c<w;c++){
             double pval = in->getpoint(c,l)[band];
-            maxtree_node n(pval);
-            data->push_back(n);
+            data->push_back(new maxtree_node(pval, idx));
             visited->push_back(false);
+            idx++;
         }
     }
 
-    nextpix = smallest_idx = min_gval(data);
-    idx_pq.push(smallest_idx);
-    idx_stack.push(smallest_idx);
-    std::cout << nextpix << " "<< smallest_idx;
-
+    nextpix = min_gval(data);
+    pixel_pq.push(nextpix);
+    pixel_stack.push(nextpix);
+    int __iter=0;
+    std::cout<<"iter:"<<__iter++<<"\n";
     do{
+        std::cout<<"iter:"<<__iter++<<"\n";
         p=nextpix;
-        std::vector<unsigned int> neighbours = get_neighbours(p,h,w);
-        for(unsigned int q : neighbours){
-            if(!visited->at(q)){
-                idx_pq.push(q);
-                visited->at(q) = true;
+        std::vector<maxtree_node *> neighbours = get_neighbours(p,data,h,w);
+        for(maxtree_node *q : neighbours){
+            if(!visited->at(q->idx)){
+                pixel_pq.push(q);
+                visited->at(q->idx) = true;
             }
-            if(data->at(q).gval > data->at(p).gval){
+            if(q->gval > p->gval){
                 break;
             }
         }
-        if(!idx_pq.empty()) nextpix = idx_pq.top();
-        if(data->at(nextpix).gval > data->at(p).gval){
-            idx_stack.push(nextpix);
+        if(!pixel_pq.empty()) nextpix = pixel_pq.top();
+        if(nextpix->gval > p->gval){
+            pixel_stack.push(nextpix);
         }else{
-            idx_pq.pop();
-            stack_top = idx_stack.top();
+            pixel_pq.remove(p);
+            if (!pixel_stack.empty()) stack_top = pixel_stack.top();
             if(p!=stack_top){
-                data->at(p).parent=stack_top;
+                p->parent=stack_top->idx;
 //                data->at(stack_top).area += data->at(p).area;
             }
-            if(!idx_pq.empty()) nextpix = idx_pq.top();
-            if(data->at(nextpix).gval < data->at(p).gval){
+            if(!pixel_pq.empty()){
+                nextpix = pixel_pq.top();
+            } else{
+                nextpix = p;
+            }
+            if(nextpix->gval < p->gval){
                 do{
                     
-                    if(!idx_stack.empty()) t = idx_stack.top();
+                    if(!pixel_stack.empty()) t = pixel_stack.top();
 
-                    idx_stack.pop();
+                    if(!pixel_stack.empty()) pixel_stack.pop();
                     if(p!=t){
-                        data->at(p).parent=t;
+                        p->parent=t->idx;
 //                        data->at(t).area += data->at(p).area;
                     }
-                }while(data->at(t).gval > data->at(nextpix).gval);
+                }while(t->gval > nextpix->gval);
             }
-            if(!idx_stack.empty()) t = idx_stack.top();
+            if(!pixel_stack.empty()) t = pixel_stack.top();
+            if(stack_top->gval < nextpix->gval){
+                pixel_stack.push(nextpix);
+            }
         }
 
-    }while(!idx_pq.empty());
+    }while(!pixel_pq.empty());
     
-    while (!idx_stack.empty()){
-        t = idx_stack.top();
-        idx_stack.pop();
+    while (!pixel_stack.empty()){
+        t = pixel_stack.top();
+        pixel_stack.pop();
         if(p!=t){
-            data->at(p).parent=t;
+            p->parent=t->idx;
 //            data->at(t).area += data->at(p).area;
         }
     }
@@ -186,7 +255,7 @@ std::vector<maxtree_node> *maxtree(VImage *in, int band = 0){
 
 int main(int argc, char **argv){
     VImage *in;
-    std::vector<maxtree_node> *t;
+    std::vector<maxtree_node*> *t;
     if (VIPS_INIT (argv[0])) 
         vips_error_exit (NULL);
 
@@ -198,6 +267,5 @@ int main(int argc, char **argv){
     t=maxtree(in);
     print_matrix(t, h, w);
     vips_shutdown();
-    std::cout << "\n";
     return 0;
 }
