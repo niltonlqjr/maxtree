cbdac9fc235391dff13683020a9abaddd8fb42dd
Nilton <niltonlqjr@gmail.com>
2026-04-05 04:30:52 -0300

    run scripts help fix

--- parallel/scripts/run_manager.sh
+++ parallel/scripts/run_manager.sh
@@ -1,6 +1,6 @@
 script_dir=$(dirname "$0")
 if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-    echo "usage $0 [manager config file] [worker config file]"
+    echo "usage $0 [manager config file]"
     exit
 elif [ -z $1 ] || ! [ -f ${manager_config} ]; then
     manager_config=./${script_dir}/../../configs/manager/manager_config_example.txt
--- parallel/scripts/run_worker.sh
+++ parallel/scripts/run_worker.sh
@@ -1,20 +1,21 @@
 worker_bin=merge_worker
 script_dir=$(dirname "$0")
 if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
-    echo "usage $0 [number of merge_worker process] [worker config file]"
+    echo "usage $0 [worker config file] [number of merge_worker process]"
     echo "each merge_worker can have multiple threads"
     exit
-elif ! [[ -z $1 ]] || [[ $1 =~ ^[0-9]+$ ]]; then
-    num_process=$1
-else
-    num_process=1
-fi
-
-if [[ -z $2 ]] || ! [[ -f ${worker_config} ]]; then
+elif [[ -z $2 ]] || ! [[ -f ${worker_config} ]]; then
     worker_config=./${script_dir}/../../configs/worker/worker_config_example.txt
     echo "No configuration file found... Using ${worker_config}"
 else
     worker_config=$2
+    
+fi
+
+if ! [[ -z $1 ]] || [[ $1 =~ ^[0-9]+$ ]]; then
+    num_process=$1
+else
+    num_process=1
 fi
 
 #worker_cmd=./${script_dir}/../exec/${worker_bin} ${worker_config}
