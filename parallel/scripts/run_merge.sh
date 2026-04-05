51e347ba2be03d75c0056a2477780110ffc48a30
Nilton <niltonlqjr@gmail.com>
2026-04-05 04:15:47 -0300

    run merge args order change

--- parallel/scripts/run_merge.sh
+++ parallel/scripts/run_merge.sh
@@ -1,15 +1,17 @@
 script_dir=$(dirname "$0")
 if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-    echo "usage $0 [manager config file] [worker config file]"
+    echo "usage $0 [worker config file] [manager config file]"
     exit
-elif [ -z $1 ] || [ -f ${manager_config} ]; then
+fi
+
+if [ -z $2 ] || [ -f ${manager_config} ]; then
     manager_config=./${script_dir}/../../configs/manager/manager_config_example.txt
     echo "No configuration manager file found... Using ${manager_config}"
 else
     manager_config=$1
 fi
 
-if [ -z $2 ] || [ -f ${worker_config} ]; then
+if [ -z $1 ] || [ -f ${worker_config} ]; then
     worker_config=./${script_dir}/../../configs/worker/worker_config_example.txt
     echo "No configuration file found... Using ${worker_config}"
 else
