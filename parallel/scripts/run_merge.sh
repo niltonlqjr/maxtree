e82f39e37068213fcde60a20bc920aca055f7752
Nilton <niltonlqjr@gmail.com>
2026-04-05 04:33:32 -0300

    run scripts fixes

--- parallel/scripts/run_merge.sh
+++ parallel/scripts/run_merge.sh
@@ -8,14 +8,14 @@ if [ -z $2 ] || [ -f ${manager_config} ]; then
     manager_config=./${script_dir}/../../configs/manager/manager_config_example.txt
     echo "No configuration manager file found... Using ${manager_config}"
 else
-    manager_config=$1
+    manager_config=$2
 fi
 
 if [ -z $1 ] || [ -f ${worker_config} ]; then
     worker_config=./${script_dir}/../../configs/worker/worker_config_example.txt
     echo "No configuration file found... Using ${worker_config}"
 else
-    worker_config=$2
+    worker_config=$1
 fi
 
 manager_bin=merge_manager
--- parallel/scripts/run_worker.sh
+++ parallel/scripts/run_worker.sh
@@ -4,16 +4,16 @@ if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
     echo "usage $0 [worker config file] [number of merge_worker process]"
     echo "each merge_worker can have multiple threads"
     exit
-elif [[ -z $2 ]] || ! [[ -f ${worker_config} ]]; then
+elif [[ -z $1 ]] || ! [[ -f ${worker_config} ]]; then
     worker_config=./${script_dir}/../../configs/worker/worker_config_example.txt
     echo "No configuration file found... Using ${worker_config}"
 else
-    worker_config=$2
+    worker_config=$1
     
 fi
 
-if ! [[ -z $1 ]] || [[ $1 =~ ^[0-9]+$ ]]; then
-    num_process=$1
+if ! [[ -z $2 ]] || [[ $2 =~ ^[0-9]+$ ]]; then
+    num_process=$2
 else
     num_process=1
 fi
